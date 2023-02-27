deff40e6c706f855618ec4e451d203f19f8dea56
paudel54 <2sansrit@gmail.com>
2023-02-27 14:57:15 +0545

    added sidebar component with mapping data from ext src

--- 1_dashboard_/src/components/Sidebar.jsx
+++ 1_dashboard_/src/components/Sidebar.jsx
@@ -4,12 +4,60 @@ import { SiShopware } from "react-icons/si";
 import { MdOutlineCancel } from "react-icons/md";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
-// import { links } from "..data/dummy";
+import { links } from "../data/dummy";
 const Sidebar = () => {
   const activeMenu = true;
+  const activeLink =
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
+  const normalLink =
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
   return (
     <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
-      Hello SideBar
+      {activeMenu && (
+        <>
+          <div className="flex justify-between">
+            <Link
+              to="/"
+              onClick={() => {}}
+              className="flex items-center gap-3 ml-3 mt-4 text-xl 
+              font-extrabold tracking-tight dark:text-white text-slate-900"
+            >
+              <SiShopware />
+              <span>Shoppy</span>
+            </Link>
+            <TooltipComponent content="Menu" position="BottomCenter">
+              <button
+                type="button"
+                onClick={() => {}}
+                className="text-xl hover:bg-light-gray rounded-full mt-4 p-3 block md:hidden"
+              >
+                <MdOutlineCancel />
+              </button>
+            </TooltipComponent>
+          </div>
+          <div className="mt-10">
+            {links.map((item) => (
+              <div key={item.title}>
+                <p className="text-gray-400 m-3 mt-4 uppercase">{item.title}</p>
+
+                {item.links.map((link) => (
+                  <NavLink
+                    to={`/${link.name}`}
+                    key={link.name}
+                    onClick={() => {}}
+                    className={({ isActive }) =>
+                      isActive ? activeLink : normalLink
+                    }
+                  >
+                    {link.icon}
+                    <span className="capitalize">{link.name}</span>
+                  </NavLink>
+                ))}
+              </div>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 };
