b6a94ccfca1792838c009283faf253a20aab4c76
paudel54 <2sansrit@gmail.com>
2023-03-01 20:03:02 +0545

    Created reuseable Component

--- spotify/src/components/sidebar/Sidebar.js
+++ spotify/src/components/sidebar/Sidebar.js
@@ -1,10 +1,25 @@
 import './sidebar.scss'
+import SidebarOption from '../sidebarOption/SidebarOption'
+import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
 
 const Sidebar = () => {
     return (
         <div className='sidebar'>
             <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png" alt="log" />
+            <SidebarOption Icon={HomeIcon} title="Home" />
+            <SidebarOption Icon={SearchIcon} title="Search" />
+            <SidebarOption Icon={LibraryMusicIcon} title="Your Library" />
+            <br />
+            <strong className='sidebar__title'>PLAYLIST</strong>
+            <hr />
+            <SidebarOption title="Country" />
+            <SidebarOption title="Pop" />
+            <SidebarOption title="Hip-Hop" />
         </div>
+
+
     )
 }
 
