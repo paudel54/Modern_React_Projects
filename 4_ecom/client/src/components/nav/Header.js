93fa38c5b97bc39787e96dc605ed2a872e7b8396
paudel54 <2sansrit@gmail.com>
2023-03-05 14:03:45 +0545

    added navigation with ant design menus

--- 4_ecom/client/src/components/nav/Header.js
+++ 4_ecom/client/src/components/nav/Header.js
@@ -1,42 +1,44 @@
 import React, { useState } from "react";
-import { Menu } from 'antd';
-import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import { Menu } from "antd";
+import {
+    AppstoreOutlined,
+    SettingOutlined,
+    UserOutlined,
+    UserAddOutlined,
+} from "@ant-design/icons";
+import { Link } from "react-router-dom";
+
+const { SubMenu, Item } = Menu;
 
 const Header = () => {
-    const [current, setCurrent] = useState([]);
-    const onMenuItemClick = (item) => {
-        setCurrent(item.key);
-        console.log('selected icon', item.key)
+    const [current, setCurrent] = useState("home");
+
+    const handleClick = (e) => {
+        // console.log(e.key);
+        // console.log(e);
+        setCurrent(e.key);
     };
-    return (<div>
-        <Menu selectedKeys={current} onClick={onMenuItemClick} mode="horizontal" items={[
-            {
-                label: "Home",
-                key: "Home",
-                icon: <MailOutlined />,
 
-            },
-            {
-                label: "Register",
-                key: "Register",
-                icon: <SettingOutlined />,
-                children: [
-                    {
-                        label: 'Men',
-                        key: 'Men'
-                    },
-                    {
-                        label: 'Women',
-                        key: 'women'
-                    }
+    return (
+        <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
+            <Item key="home" icon={<AppstoreOutlined />}>
+                <Link to="/">Home</Link>
+            </Item>
 
-                ]
-            }
+            <Item key="register" icon={<UserAddOutlined />} className="float-right">
+                <Link to="/register">Register</Link>
+            </Item>
 
-        ]}>
+            <Item key="login" icon={<UserOutlined />} className="float-right">
+                <Link to="/login">Login</Link>
+            </Item>
+
+            {/* <SubMenu icon={<SettingOutlined />} title="Username">
+                <Item key="setting:1">Option 1</Item>
+                <Item key="setting:2">Option 2</Item>
+            </SubMenu> */}
         </Menu>
-    </div>);
-}
+    );
+};
 
-
-export default Header
+export default Header;
