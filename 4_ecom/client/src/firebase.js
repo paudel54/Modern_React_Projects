ba06a54aa2ea30aacc21fcd705d5de2386cde8de
paudel54 <2sansrit@gmail.com>
2023-03-05 15:42:11 +0545

    established firebase auth

---
+++ 4_ecom/client/src/firebase.js
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase';
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAK25jwZuXVCYVYPYzlgjtEofOr9pdJ2LE",
+    authDomain: "ecom-2aa63.firebaseapp.com",
+    projectId: "ecom-2aa63",
+    storageBucket: "ecom-2aa63.appspot.com",
+    messagingSenderId: "282529697090",
+    appId: "1:282529697090:web:ac497cf2e872d1b139ccdb"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth()
+export const googleAuthProvider = new firebase.auth.googleAuthProvider();
+// start using firebase auth features. 
