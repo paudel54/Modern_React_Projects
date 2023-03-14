3b57f677396743bb1bbeefe4b0eefa5f3d43033d
paudel54 <2sansrit@gmail.com>
2023-03-14 17:38:38 +0545

    Created category Schema

---
+++ 4_ecom/server/models/category.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+// const { ObjectId } = mongoose.Schema
+
+
+const categorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        //user might have white space we want to eliminate or ignore into database: // react woulde be //react
+        trim: true,
+        required: true,
+        //minimum length must be 3 character if not would throw message as too short
+        minlength: [3, 'Too short'],
+        maxlength: [32, 'Too long'],
+    },
+    //slug would be genereated on own controller not from frontend> 
+    slug: {
+        type: String,
+        unique: true,
+        lowercase: true,
+        index: true,
+    }
+}, { timestamps: true });
+
+//create mongoose module which helps to interat with database: model name and 2nd paramater is category is schema
+module.exports = mongoose.model('Category', categorySchema);
