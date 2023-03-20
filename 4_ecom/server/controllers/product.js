f478da8d8b500eb586fe25992f1eb54375c0e100
paudel54 <2sansrit@gmail.com>
2023-03-20 17:44:19 +0545

    set endpoint for products

--- 4_ecom/server/controllers/product.js
+++ 4_ecom/server/controllers/product.js
@@ -11,9 +11,15 @@ exports.create = async (req, res) => {
     } catch (err) {
         console.log(err);
         // res.status(400).send("Create product failed");
-        //sending error message to frontend to show it on proper way
+        //sending error message to frontend to show it on proper way 
         res.status(400).json({
             err: err.message,
         });
     }
 };
+
+exports.read = async (req, res) => {
+    //query to database: shows list of products in from db 
+    let products = await Product.find({});
+    res.json(products);
+}
