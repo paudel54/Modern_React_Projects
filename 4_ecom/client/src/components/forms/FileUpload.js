8b6427809ad59f29cf676779899935bba3e43975
paudel54 <2sansrit@gmail.com>
2023-03-21 16:08:03 +0545

    File resize in react

--- 4_ecom/client/src/components/forms/FileUpload.js
+++ 4_ecom/client/src/components/forms/FileUpload.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import Resizer from 'react-image-file-resizer';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+
 
 const FileUpload = () => {
+    const { user } = useSelector((state) => ({ ...state }));
     const fileUploadAndResize = (e) => {
-        // console.log(e.target.files);
+        console.log(e.target.files);
         //resize
+        //if upload was single then would be much easier: let files = e.target.files[0];
+        //multiple upload get all files and loop on all 
+        let files = e.target.files;
+        //files contains object with files info: {name:..}
+        if (files) {
+            for (let i = 0; i < files.length; i++) {
+                Resizer.imageFileResizer(files[i], 720, 720, 'JPEG', 100, 0, (uri) => {
+                    console.log(uri);
+                },
+                    "base64"
+                );
+            }
+        }
         //send back to server to upload to cloudinary- cloudinary will give to server- server to cient
         //set url to images [] array in parent componet- productCreate
     }
