57fc0b8522084cdff9617b8bed06514accb8ab70
paudel54 <2sansrit@gmail.com>
2023-03-24 12:40:06 +0545

    fetched products in homepage

--- 4_ecom/client/src/pages/Home.js
+++ 4_ecom/client/src/pages/Home.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getProductsByCount } from '../components/functions/product';
 
 const Home = () => {
+    //state to hold the response of products from db through backend
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        loadAllProducts();
+    }, [])
+
+    const loadAllProducts = () => {
+        getProductsByCount(1)
+            .then(r => {
+                setProducts(r.data);
+            });
+    };
+
     return (
         <div>
             Home
+            {JSON.stringify(products)}
         </div>
     )
 }
