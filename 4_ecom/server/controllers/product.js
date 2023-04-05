2a1e5930d6d3ab87fc8462872eb79eda6a2a3f9f
paudel54 <2sansrit@gmail.com>
2023-04-05 12:31:21 +0545

    Created rating backend sys, client side compo init

--- 4_ecom/server/controllers/product.js
+++ 4_ecom/server/controllers/product.js
@@ -217,12 +217,42 @@ const handleCategory = async (req, res, category) => {
     }
 }
 
+const handleStar = (req, res, stars) => {
+    //mongodb project Aggregation.
+    Product.aggregate([
+        {
+            //generate a project based on document 
+            $project: {
+                document: "$$ROOT",
+                //generated floor Average
+                floorAverage: {
+                    $floor: { $avg: "$ratings.star" }
+                }
+            }
+        },
+        {
+            $match: { floorAverage: stars }
+        }
+    ])
+        .limit(12)
+        .exec((err, aggregates) => {
+            if (err) console.log('AGGREGATES ERROR', err)
+            Product.find({ _id: aggregates })
+                .populate('category')
+                .populate('subs')
+                .exec((err, products) => {
+                    if (err) console.log('PRODUCT AGGREGATE ERROR', err)
+                    res.json(products);
+                });
+        })
+};
+
 
 //everytime the query input we might get be different
 //for example one can be from slider, next be form , next filter star and so on!
 exports.searchFilters = async (req, res) => {
 
-    const { query, price, category } = req.body
+    const { query, price, category, stars } = req.body
     if (query) {
         console.log('query', query)
         await handleQuery(req, res, query);
@@ -237,6 +267,11 @@ exports.searchFilters = async (req, res) => {
     if (category) {
         console.log('Category------>', category)
         await handleCategory(req, res, category);
+    }
+
+    if (stars) {
+        console.log('Stars -------->', stars)
+        await handleStar(req, res, stars)
     }
 }
 
