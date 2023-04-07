2b80a2ac1de074b2a02ec9e38d38c3c6f0f7ad41
paudel54 <2sansrit@gmail.com>
2023-04-07 07:20:31 +0545

    Created order summary to checkout

--- 4_ecom/client/src/pages/Cart.js
+++ 4_ecom/client/src/pages/Cart.js
@@ -7,11 +7,42 @@ const Cart = () => {
     const { cart, user } = useSelector((state) => ({ ...state }));
     //TO UPDATE REDUX
     const dispatch = useDispatch()
+
+    const getTotal = () => {
+        return cart.reduce((current, next) => {
+            return current + next.count * next.price
+        }, 0)
+    }
+
     return (
         <div>
             <div>
-                <h4 className='text-red-600 text-2xl text-center'>Card page here.......</h4>
-                {JSON.stringify(cart)}
+                {/* {JSON.stringify(cart)} */}
+                <h4 className='text-xl font-bold'>Cart / {cart.length} Product </h4>
+            </div>
+            <div className='grid grid-cols-12'>
+                <div className='col-span-8'>
+                    {!cart.length ? <p>No Products in Cart. : <link to="/shop">Continue Shopping</link> </p> : ("Show Cart Items")}
+                </div>
+
+                <div className='col-span-4 space-y-3'>
+                    <h4 className='text-2xl font-bold -mt-5'>Order Summary</h4>
+                    <hr />
+                    <p>Products</p>
+                    {cart.map((c, i) => (
+                        <div key={i}>
+                            <p> {c.title} x {c.count} = ${c.price * c.count}</p>
+                        </div>
+                    ))}
+                    <hr />
+                    Total: <b>${getTotal()}</b>
+                    <hr />
+                    {
+                        user ? (<button className='text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 mt-5 outline-none'> Proceed to Checkout</button>)
+                            : (<button className='text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 mt-5 outline-none'>Login to Checkout</button>)
+                    }
+                </div>
+
             </div>
         </div>
     )
---
+++ 4_ecom/client/test.js
@@ -0,0 +1,14 @@
+//Js Object=>Pass by reference
+//All we are doing this is to save Memory. 
+
+let obj1 = {
+    name: 'Mark',
+    password: '123'
+}
+let obj2 = obj1;
+//I want to change obj2 password to 333
+obj2.password = '333';
+console.log('log from obj1', obj1); ///output { name: 'Mark', password: '333' }
+console.log('log from obj2', obj2);//{ name: 'Mark', password: '333' }
+//Object1 and Object2 are pointing to same
+//memory locations. 
