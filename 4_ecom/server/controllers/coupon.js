26511d1ae71ecd330bb420ec29653cd7763dae1c
paudel54 <2sansrit@gmail.com>
2023-04-10 11:45:20 +0545

    Created Coupon Routes and Controller methods

---
+++ 4_ecom/server/controllers/coupon.js
@@ -0,0 +1,30 @@
+const Coupon = require('../models/coupon');
+
+//create, remove, list
+
+exports.create = async (req, res) => {
+    try {
+        const { name, expiry, discount } = req.body
+        //take the info form client side and save to db and send response back to client
+        res.json(await new Coupon({ name, expiry, discount }).save());
+    } catch (e) {
+        console.log(e)
+    }
+}
+//remove
+exports.remove = async (req, res) => {
+    try {
+        res.json(await Coupon.findByIdAndDelete(req.params.couponId).exec());
+    } catch (e) {
+
+    }
+}
+
+//list
+exports.list = async (req, res) => {
+    try {
+        res.json(await Coupon.find({}).sort({ createdAt: -1 }).exec());
+    } catch (e) {
+
+    }
+}
