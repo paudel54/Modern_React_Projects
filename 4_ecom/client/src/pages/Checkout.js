4f0db398fcefba39e2ede19af361d2ad2fce2632
paudel54 <2sansrit@gmail.com>
2023-04-11 21:56:35 +0545

    Added coupon Error and Success Message

--- 4_ecom/client/src/pages/Checkout.js
+++ 4_ecom/client/src/pages/Checkout.js
@@ -18,7 +18,7 @@ const Checkout = () => {
     //coupon on state from input
     const [coupon, setCoupon] = useState('');
     //one we get response form server for discounted price handle it on state
-    const [totalAfterDiscount, setTotalAfterDiscount] = useState('');
+    const [totalAfterDiscount, setTotalAfterDiscount] = useState(0);
     const [discountError, setDiscountError] = useState('');
 
     useEffect(() => {
@@ -47,32 +47,23 @@ const Checkout = () => {
             .then(res => {
                 setProducts([])
                 setTotal(0)
+                //clearing the state out
+                setTotalAfterDiscount(0)
+                setCoupon('');
                 toast.message('Cart is empty. Continue Shopping..')
             })
     }
 
-    // const saveAddressToDb = () => {
-    //     //console.log(address);
-    //     //<p>Rampur</p><p><strong>Chitrapur</strong></p><p>Nepal</p> Rich text element
-    //     saveUserAddress(user.token, address)
-    //         .then(res => {
-    //             if (res.data.ok) {
-    //                 setAddressSaved(true);
-    //                 console.log('address is saved into DB')
-    //                 toast.success("Address Saved");
-    //             }
-    //         })
-    // }
-
     const saveAddressToDb = () => {
         // console.log(address);
+        //     //<p>Rampur</p><p><strong>Chitrapur</strong></p><p>Nepal</p> Rich text element
         saveUserAddress(user.token, address).then((res) => {
             console.log('Response after saving on DB', res)
             setAddressSaved(true);
-            // if (res.data.ok) {
-            //     setAddressSaved(true);
-            //     toast.success("Address saved");
-            // }
+            if (res.data.ok) {
+                setAddressSaved(true);
+                toast.success("Address saved");
+            }
         });
     };
 
@@ -97,20 +88,38 @@ const Checkout = () => {
             <hr />
         </>
     }
+
     const applyDiscountCoupon = () => {
         console.log("send coupon to backend", coupon);
         applyCoupon(user.token, coupon).then((res) => {
             console.log("RES ON COUPON APPLIED", res);
+            if (res.data) {
+                setTotalAfterDiscount(res.data.totalAfterDiscount);
+                //update redux coupon applied
+            }
+            if (res.data.err) {
+                setDiscountError(res.data.err)
+                //update redux coupon applied
+            }
         });
     };
 
-
     const showApplyCoupon = () => {
         return <>
-            <input type='text' onChange={(e) => setCoupon(e.target.value)} value={coupon} className='bg-green-50 border border-green-500  placeholder-black-700  text-sm rounded-lg block w-1/4 p-2.5 outline-none' />
+            <input type='text'
+                onChange={
+                    (e) => {
+                        setCoupon(e.target.value);
+                        setDiscountError('');
+                    }
+                }
+                value={coupon}
+                className='bg-green-50 border border-green-500  placeholder-black-700  text-sm rounded-lg block w-1/4 p-2.5 outline-none' />
             <button className='text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 mt-5 outline-none' onClick={applyDiscountCoupon}>Apply</button>
         </>
     }
+
+
 
     return (
         <div>
@@ -124,9 +133,10 @@ const Checkout = () => {
 
                     <div>{showApplyCoupon()}</div>
                     <br />
+                    {/* ***************conditional render to shoow error message*/}
                     {discountError && <p className='text-red-600 text-xl font-bold p-2'>{discountError}</p>}
 
-                    {JSON.stringify(discountError)}
+                    {/* {JSON.stringify(discountError)} */}
                 </div>
                 <div className='col-span-6 space-y-2 p-2'>
                     <h4 className='text-2xl font-bold'>Order Summary</h4>
@@ -137,6 +147,12 @@ const Checkout = () => {
                     <hr />
                     {showProductSummary()}
                     <p>Cart Total: {total}</p>
+
+                    {totalAfterDiscount > 0 && (
+                        <p className='bg-green-300 p-1'> Discounted: Total Payable
+                            <span className='font-bold'> ${totalAfterDiscount}</span>
+                        </p>
+                    )}
 
                     <div>
                         <div>
@@ -155,5 +171,8 @@ const Checkout = () => {
 }
 
 export default Checkout
+
+
+
 
 
