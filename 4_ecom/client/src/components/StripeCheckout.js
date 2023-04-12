f24d4940bf9b9a04a4e8198dd2372d94ca637916
paudel54 <2sansrit@gmail.com>
2023-04-12 16:10:19 +0545

    Processed the discount on coupon redeem applied client/server

--- 4_ecom/client/src/components/StripeCheckout.js
+++ 4_ecom/client/src/components/StripeCheckout.js
@@ -4,6 +4,10 @@ import { useSelector, useDispatch } from 'react-redux';
 import { createPaymentIntent } from './functions/stripe';
 import { useNavigate, Link } from 'react-router-dom';
 
+import { Card } from 'antd';
+import { DollarOutlined, CheckOutlined } from '@ant-design/icons';
+import Laptop from '../images/computer/ban1.png'
+
 
 const StripeCheckout = () => {
     const navigate = useNavigate();
@@ -17,6 +21,11 @@ const StripeCheckout = () => {
     //useEffect will make backend request to get clientSecret and put into State clientSecreat value 
     const [clientSecret, setClientSecret] = useState('');
 
+    //state variable to store info from backend response
+    const [cartTotal, setCartTotal] = useState(0);
+    const [totalAfterDiscount, setTotalAfterDiscount] = useState(0);
+    const [payable, setPayable] = useState(0);
+
     const stripe = useStripe();
     const element = useElements();
 
@@ -27,6 +36,11 @@ const StripeCheckout = () => {
             .then(res => {
                 console.log('create payment intent', res.data);
                 setClientSecret(res.data.clientSecret);
+                //additional Response Received from successful payment
+                //update state from backend to state variable
+                setCartTotal(res.data.cartTotal);
+                setTotalAfterDiscount(res.data.totalAfterDiscount);
+                setPayable(res.data.payable);
             })
     }, []);
 
@@ -88,15 +102,45 @@ const StripeCheckout = () => {
     return (
         <div>
             <div>
-                <p className={succeeded ? 'bg-green-300 p-2 text-sm border rounded text-gray-600' : 'collapse'}>
-                    Payment Successful.<Link to="/user/history"> <span className='text-blue-600 font-bold'>&nbsp; Checkout Purchase History </span> </Link>
-                </p>
+                {
+                    !succeeded && <div> {coupon && totalAfterDiscount !== undefined ? (<p className='text-green-600 p-2 bg-orange-200 rounded-xl text-xl font-bold'> {`Total After Discount: $${totalAfterDiscount}`} </p>) : (<p className='text-red-600 bg-orange-200 rounded-xl text-xl font-bold'> No Coupon Applied</p>)} </div>
+                }
+                <div className='text-center pb-5'>
+                    <Card
+                        cover={<img alt='icon' src={Laptop} style={{
+                            height: '200px',
+                            objectFit: 'cover',
+                            marginBottom: '-50px'
+                        }}
+                        />}
+                        actions={[
+                            <>
+                                <DollarOutlined className='' /> <br />
+                                Total: $ {cartTotal}
+                            </>,
+                            <>
+                                <CheckOutlined className='' /> <br />
+                                Total Payable: $ {(payable / 100).toFixed(2)}
+                            </>,
+
+                        ]}
+                    />
+
+
+                </div>
+
+
                 <form id="payment-form" className='stripe-form' onSubmit={handleSubmit}>
                     <CardElement id="card-element" options={cardStyle} onChange={handleChange} />
                     <button className='stripe-button' disabled={processing || disabled || succeeded}>
                         <span id="button-text">{processing ? <div className='spinner' id="spinner"></div> : "Pay"}</span>
                     </button>
                     {error && <div> {error}</div>}
+                    <br />
+                    <p className={succeeded ? 'bg-green-300 p-2 text-sm border rounded text-gray-600' : 'collapse'}>
+                        Payment Successful.<Link to="/user/history"> <span className='text-blue-600 font-bold'>&nbsp; Checkout Purchase History </span> </Link>
+                    </p>
+
                 </form>
                 {/* {JSON.stringify(clientSecret)} */}
                 {/* {JSON.stringify(succeeded)} */}
