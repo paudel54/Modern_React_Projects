834cc359e0e87a21d4fa13de4b3e048211709f51
paudel54 <2sansrit@gmail.com>
2023-04-12 09:52:02 +0545

    Created stripe payment client side

--- 4_ecom/client/src/pages/Payment.js
+++ 4_ecom/client/src/pages/Payment.js
@@ -1,15 +1,28 @@
 import React from 'react'
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+//load Stripe Outside of Component render to avoid recreating Stripe object on every render
+const promise = loadStripe(process.env.REACT_APP_STRIPE_KEY);
 
 const Payment = () => {
     return (
         <div>
             <div className='font-bold text-xl text-center'>
-                Payment with Stripe!
+                Complete Your Purchase!
             </div>
-            <div className='font-bold text-xl text-center text-green-700'>
-                Complete Your Purchase
-            </div>
+            <Elements stripe={promise}>
+                <div className='grid grid-cols12'>
+                    <div className='col-span-8'>
+                        <div className='font-bold text-xl text-center text-green-700'>
+                            Stripe Checkout Component
+                        </div>
+                    </div>
+                    <div className='col-span-4'>
 
+                    </div>
+                </div>
+            </Elements>
         </div>
     )
 }
