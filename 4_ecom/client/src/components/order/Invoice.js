514cce6a9b21165eb8f14fdba07f729145a3b3aa
paudel54 <2sansrit@gmail.com>
2023-04-13 15:30:56 +0545

    Added invoice download with Pdf

--- 4_ecom/client/src/components/order/Invoice.js
+++ 4_ecom/client/src/components/order/Invoice.js
@@ -17,8 +17,52 @@ const Invoice = ({ order }) => (
                 ~ {new Date().toLocaleString()} ~
             </Text>
             <Text style={styles.title}>Order Invoice</Text>
-            <Text style={styles.author}>React Redux Ecommerce</Text>
+            <Text style={styles.author}>Paieu</Text>
             <Text style={styles.subtitle}>Order Summary</Text>
+            <Table>
+                <TableHeader>
+                    <TableCell>Title</TableCell>
+                    <TableCell>Price</TableCell>
+                    <TableCell>Quantity</TableCell>
+                    <TableCell>Brand</TableCell>
+                    <TableCell>Color</TableCell>
+                </TableHeader>
+            </Table>
+
+            <Table data={order.products}>
+                <TableBody>
+                    <DataTableCell getContent={(x) => x.product.title} />
+                    <DataTableCell getContent={(x) => `$${x.product.price}`} />
+                    <DataTableCell getContent={(x) => x.count} />
+                    <DataTableCell getContent={(x) => x.product.brand} />
+                    <DataTableCell getContent={(x) => x.product.color} />
+                </TableBody>
+            </Table>
+            <Text style={styles.text}>
+                <Text>
+                    Date: {"               "}
+                    {new Date(order.paymentIntent.created * 1000).toLocaleString()}
+                </Text>
+                {"\n"}
+                <Text>
+                    Order Id: {"         "}
+                    {order.paymentIntent.id}
+                </Text>
+                {"\n"}
+                <Text>
+                    Order Status: {"  "}
+                    {order.orderStatus}
+                </Text>
+                {"\n"}
+                <Text>
+                    Total Paid: {"       "}
+                    {order.paymentIntent.amount}
+                </Text>
+            </Text>
+
+            <Text style={styles.footer}> ~ Thank you for shopping with us ~ </Text>
+
+
         </Page>
     </Document>
 );
@@ -32,6 +76,7 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 24,
         textAlign: "center",
+
     },
     author: {
         fontSize: 12,
