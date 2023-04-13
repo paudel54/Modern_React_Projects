b5f79d128fb458abfc08c49a4e2747584e1c4d3c
paudel54 <2sansrit@gmail.com>
2023-04-13 19:07:34 +0545

    Loading Orders in admin Dashboard

--- 4_ecom/client/src/pages/admin/AdminDashboard.js
+++ 4_ecom/client/src/pages/admin/AdminDashboard.js
@@ -1,11 +1,42 @@
 import AdminNav from '../../components/nav/AdminNav';
+import { useState, useEffect } from 'react';
+//Hit the endpoint.
+import { getOrders, changeStatus } from '../../components/functions/admin';
+//Access Redux store.
+import { useSelector, useDispatch } from 'react-redux';
+import { toast } from 'react-toastify';
+
 const AdminDashboard = () => {
+  const [orders, setOrders] = useState([]);
+  const { user } = useSelector((state) => ({ ...state }));
+
+  useEffect(() => {
+    loadOrders();
+  }, []
+  )
+
+  const loadOrders = () =>
+    getOrders(user.token).then((res) => {
+      console.log(JSON.stringify(res.data, null, 4));
+      setOrders(res.data);
+    });
+
+  //when selecting from dropdown updates occures and change is reflected::>
+  const handleStatusChange = (orderId, orderStatus) => {
+    changeStatus(orderId, orderStatus, user.token).then((res) => {
+      toast.success("Status updated");
+      //change is reflected
+      loadOrders();
+    });
+  };
+
   return (
     <div className='bg-green-200   grid  grid-cols-12 '>
       <div className='col-span-2'><AdminNav /></div>
 
       <div className='col-span-10'>
         <div className=''>Admin Dashboard</div>
+        {JSON.stringify(orders)}
       </div>
     </div>
   )
